c0eb97542ad581d7448e5507310f9e6dadb407bb
ladex <ladipoilori@yahoo.com>
2016-03-27 17:23:38 +0000

    Implemented angular google chart

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@
 /*global angular*/
 (function(){
     'use strict';
-    angular.module('myApp',['myApp.controllers','myApp.directives']);
+    angular.module('myApp',['googlechart','myApp.controllers','myApp.directives']);
 })();
--- js/controller.js
+++ js/controller.js
@@ -2,7 +2,58 @@
 (function() {
     'use strict';
 
-    angular.module('myApp.controllers', []).controller('BarChartCtrl', BarChartCtrl);
+    angular.module('myApp.controllers', [])
+    .controller('BarChartCtrl', BarChartCtrl)
+     .controller('ChartCtrl', ChartCtrl);
+    
+
+    function ChartCtrl() {
+        var vm = this;
+
+        vm.chartObject = {
+            type: 'PieChart',
+            data: {
+                "cols": [{
+                    label: 'frameworks',
+                    type: 'string'
+                }, {
+                    label: 'shares',
+                    type: 'number'
+                }],
+                "rows": [{
+                    c: [{
+                        v: "Angular.js"
+                    }, {
+                        v: 54
+                    }]
+                }, {
+                    c: [{
+                        v: "Backbone.js"
+                    }, {
+                        v: 9
+                    }]
+                }, {
+                    c: [{
+                        v: "Ember.js"
+                    }, {
+                        v: 2
+                    }]
+                }, {
+                    c: [{
+                        v: "Knockout"
+                    }, {
+                        v: 21
+                    }]
+                }, {
+                    c: [{
+                        v: "Others"
+                    }, {
+                        v: 14
+                    }]
+                }]
+            }
+        };
+    }
 
     function BarChartCtrl() {
         var vm = this;
@@ -13,21 +64,28 @@
             gap: 10
         };
 
-        vm.bars = [{color: 'blue', percentage: 50 }, 
-        {color: 'orange',percentage: 60 }, 
-        {color: 'red', percentage: 10 }];
-        
-        vm.setContainer = function(){
-            return{
+        vm.bars = [{
+            color: 'blue',
+            percentage: 50
+        }, {
+            color: 'orange',
+            percentage: 60
+        }, {
+            color: 'red',
+            percentage: 10
+        }];
+
+        vm.setContainer = function() {
+            return {
                 'padding-right': vm.container.gap + 'px',
                 'width': vm.container.width + 'px',
                 'height': vm.container.height + 'px'
             };
         };
-        
-        vm.setDetails = function(bar, index){
+
+        vm.setDetails = function(bar, index) {
             var barWidth = (vm.container.width / vm.bars.length) - vm.container.gap;
-            return{
+            return {
                 height: bar.percentage + '%',
                 background: bar.color,
                 width: barWidth + 'px',
